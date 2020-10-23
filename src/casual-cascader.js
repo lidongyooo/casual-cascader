0816ecb752c1d20651d04037dd008a55e5f4a371
lidong <2271439559@qq.com>
2020-10-23 17:39:52 +0800

    基本完成~差回调函数

--- src/casual-cascader.js
+++ src/casual-cascader.js
@@ -1,7 +1,7 @@
 /*!
     @Address：https://github.com/LiDong525/casual-cascader.git
     @Name: casual-cascader
-    @Description：无限级联搜索
+    @Description：无限级联选择
     @Author: lidong
     @License：MIT
 */
@@ -11,120 +11,168 @@
 
 
 const CASUAL_PANEL = '<div class="casual-panel">',
-      CASUAL_MENU = '<div class="casual-menu">',
-      CASUAL_MENU_LIST = '<ul class="casual-menu-list">',
-      DIV_END = '</div>'
+    CASUAL_MENU = '<div class="casual-menu">',
+    CASUAL_MENU_LIST = '<ul class="casual-menu-list">',
+    DIV_END = '</div>'
 
 const CASUAL_PANEL_CLASS = 'casual-panel',
-      CASUAL_MENU_CLASS = 'casual-menu',
-      CASUAL_MENU_LIST_CLASS = 'casual-menu-list',
-      CASUAL_MENU_LIST_ITEM_CLASS = 'casual-menu-list-item'
+    CASUAL_MENU_CLASS = 'casual-menu',
+    CASUAL_MENU_LIST_CLASS = 'casual-menu-list',
+    CASUAL_MENU_LIST_ITEM_CLASS = 'casual-menu-list-item',
+    ACTIVE_CLASS = 'casual-menu-list-item_active',
+    ICON_UP_CLASS = 'casual-icon-up',
+    ICON_DOWN_CLASS = 'casual-icon-down'
 
 let casualIndex = 1,
-    configs  = []
+    casualConfigs = []
 
-let CasualCascader = function(element,data,options){
-    let _this =  this
+let CasualCascader = function (element, data, options) {
+    let _this = this
     options = options || {}
 
     //初始配置
     let config = {
-        names : ['province','city','district','street'], //input的name值,同时也代表渲染的层级~
-        placeHolder : '省/市/区/街道',
-        size : '14',
-        color : '#606266',
-        activeColor : '#409eff',
-        width : '180',
-        height : '190',
-        borderColor : '#e4e7ed',
-        backgroundColor : 'white',
-        children : 'children', //子级字段名称
-        visibility : 'name', //显示字段名称
-        close : true,
-        isShow : false //是否显示
+        names: ['province', 'city', 'district', 'street'], //input的name值
+        delimiter: '/',
+        value: '',
+        values: [],
+        placeHolder: '省/市/区/街道',
+        size: '14px',
+        color: '#606266',
+        activeColor: '#409eff',
+        width: '180px',
+        height: '190px',
+        borderColor: '#e4e7ed',
+        backgroundColor: 'white',
+        children: 'children', //子级字段名称
+        visibility: 'name', //显示字段名称
+        close: true,
+        isShow: false, //是否显示
+        iconLeft: 0,
+        iconTop: 0,
+        iconSize : '16px',
+        iconColor : 'black'
     }
 
-    if( !(element) ){
+    if (!(element) || element.tagName !== 'INPUT') {
         throw '请给定一个input元素'
-    }else if( !(data) ){
+    } else if (!(data)) {
         throw '请给定数据'
     }
 
     options.element = element
     options.data = data
-    _this.bindConfigs(CASUAL_PANEL_CLASS+'-'+casualIndex,Object.assign(config,options))
+    _this.bindcasualConfigs(CASUAL_PANEL_CLASS + '-' + casualIndex, Object.assign(config, options))
     _this.render()
+    _this.default()
+    _this.show()
 
     casualIndex++
 }
 
-CasualCascader.prototype.bindConfigs = function(key,config){
-    configs[key] = config
-    configs[key]['casualIndex'] = CASUAL_PANEL_CLASS+'-'+casualIndex
+CasualCascader.prototype.bindcasualConfigs = function (key, config) {
+    casualConfigs[key] = config
+    casualConfigs[key]['casualIndex'] = CASUAL_PANEL_CLASS + '-' + casualIndex
+}
+
+CasualCascader.prototype.setValue = function(element){
+    this.default(casualConfigs[element.nextSibling.nextSibling.classList[1]])
+}
+
+CasualCascader.prototype.default = function(config){
+    let index = 0,
+        flag = true
+
+    config = config || casualConfigs[CASUAL_PANEL_CLASS + '-' + casualIndex]
+
+    config.value = config.value || config['element'].value
+    config.values = config.values.length === 0 ? config.value.split(config['delimiter']) : config.values
+
+    for (let value of config.values){
+        let items = getElementsByClassName(config['casualIndex']).getElementsByClassName(CASUAL_MENU_CLASS)[index].getElementsByClassName(CASUAL_MENU_LIST_ITEM_CLASS)
+        for (let item of items){
+            if(item.getAttribute('data-value') === value){
+                item.click()
+                flag = true
+                break
+            }else{
+                flag = false
+            }
+        }
+        if(!flag){
+            break
+        }
+        index++
+    }
 }
 
 //渲染
 CasualCascader.prototype.render = function () {
-    let _this =  this,
-        config = configs[CASUAL_PANEL_CLASS+'-'+casualIndex]
+    let _this = this,
+        config = casualConfigs[CASUAL_PANEL_CLASS + '-' + casualIndex],
+        spaceAttribute = {
+            left: _this.getElementLeft(config['element']),
+            top: _this.getElementTop(config['element']),
+            height: config['element']['offsetHeight'],
+            width: config['element']['offsetWidth']
+        },
+        content = []
 
-    let content  = [];
-    content.push(CASUAL_PANEL)
-    content = content.concat(generateMenu(config['data'],config))
+    config['element'].setAttribute('placeHolder', config['placeHolder'])
+
+    content.push('<i class="' + ICON_UP_CLASS + '" style="color:'+config['iconColor']+';font-size: '+config['iconSize']+';position: absolute;left: ' + (spaceAttribute['left'] + spaceAttribute['width'] - 25 + parseInt(config['iconLeft'])) + 'px;top: ' + (spaceAttribute['top']+ spaceAttribute['height']/2 - 11 + parseInt(config['iconTop'])) + 'px"></i>' + CASUAL_PANEL)
+    content = content.concat(generateMenu(config['data'], config))
     content.push(DIV_END)
-    insertHTML(config['element'],'afterend',content.join(''))
+    insertHTML(config['element'], 'afterend', content.join(''))
 
     //添加类名
-    config['element'].nextSibling.classList.add(config['casualIndex'])
-    _this.show()
-    _this.panelStyle(config)
+    config['element'].nextSibling.nextSibling.classList.add(config['casualIndex'])
+
+    _this.panelStyle(config, spaceAttribute)
 
     //绑定事件
-    config['element'].addEventListener('click',_this.show)
-
-    document.addEventListener('click',hidePanel)
+    config['element'].addEventListener('click', _this.show)
+    document.addEventListener('click', hidePanel)
 }
 
-CasualCascader.prototype.panelStyle = function (config) {
-    let _this = this,
-        spaceAttribute = {
-            left : _this.getElementLeft(config['element']),
-            top : _this.getElementTop(config['element'])+config['element'].offsetHeight+8,
-            height : config['element']['offsetHeight'],
-            width : config['element']['offsetHeight']
-        }
+CasualCascader.prototype.panelStyle = function (config, spaceAttribute) {
+    let _this = this
 
     //panel 样式设置
-    let panel = document.getElementsByClassName(config['casualIndex'])[0]
+    let panel = getElementsByClassName(config['casualIndex'])
     let styles = {
-        "position" : 'absolute',
-        "left" : spaceAttribute['left']+'px',
-        "top" : spaceAttribute['top']+'px',
-        "font-size" : config['size'],
-        'color' : config['color']
+        "position": 'absolute',
+        "left": spaceAttribute['left'] + 'px',
+        "top": (spaceAttribute['top'] + config['element'].offsetHeight + 8) + 'px',
+        "font-size": config['size'],
+        'color': config['color']
     }
-    _this.setStyle(panel,styles)
+    _this.setStyle(panel, styles)
 }
 
-CasualCascader.prototype.setStyle = function(element,styles){
-    for (let key in styles){
+CasualCascader.prototype.setStyle = function (element, styles) {
+    for (let key in styles) {
         element['style'][key] = styles[key]
     }
 }
 
 //显示隐藏
 CasualCascader.prototype.show = function (event) {
-    let panelIndex = event !== undefined ? event.target.nextSibling.classList[1] : configs[CASUAL_PANEL_CLASS+'-'+casualIndex]['element'].nextSibling.classList[1]
+    let panelIndex = event !== undefined ? event.target.nextSibling.nextSibling.classList[1] : casualConfigs[CASUAL_PANEL_CLASS + '-' + casualIndex]['element'].nextSibling.nextSibling.classList[1],
+        config = casualConfigs[panelIndex]
 
-    if(event !== undefined){
+    if (event !== undefined) {
         event.stopPropagation()
-        configs[panelIndex]['isShow'] = configs[panelIndex]['isShow'] ? false : true
+        config['isShow'] = config['isShow'] ? false : true
     }
 
-    if(configs[panelIndex]['isShow']){
-        document.getElementsByClassName(panelIndex)[0]['style']['display'] = 'inline-flex'
-    }else{
-        document.getElementsByClassName(panelIndex)[0]['style']['display'] = 'none'
+    if (config['isShow']) {
+        getElementsByClassName(panelIndex)['style']['display'] = 'inline-flex'
+        iconChanged(getElementsByClassName(panelIndex).previousSibling, 'up')
+    } else {
+        getElementsByClassName(panelIndex)['style']['display'] = 'none'
+        iconChanged(getElementsByClassName(panelIndex).previousSibling, 'down')
+        setValues(config)
     }
 }
 
@@ -132,7 +180,7 @@ CasualCascader.prototype.getElementLeft = function (element) {
     let actualLeft = element.offsetLeft
     let current = element.offsetParent
 
-    while (current !== null){
+    while (current !== null) {
         actualLeft += current.offsetLeft
         current = current.offsetParent
     }
@@ -144,7 +192,7 @@ CasualCascader.prototype.getElementTop = function (element) {
     let actualTop = element.offsetTop
     let current = element.offsetParent
 
-    while (current !== null){
+    while (current !== null) {
         actualTop += current.offsetTop
         current = current.offsetParent
     }
@@ -152,18 +200,18 @@ CasualCascader.prototype.getElementTop = function (element) {
     return actualTop
 }
 
-function removeNextSiblings(element,currCasualMenu,casualMenus){
+function removeNextSiblings(element, currCasualMenu, casualMenus) {
 
     let casualMenusLength = casualMenus.length,
         flag = false,
         counter = 0
 
-    for (let index = 0; index < casualMenusLength; index++){
-        if(flag === false){
+    for (let index = 0; index < casualMenusLength; index++) {
+        if (flag === false) {
             flag = currCasualMenu === casualMenus[index]
         }
-        if(flag && currCasualMenu !== casualMenus[index]){
-            casualMenus[index-counter].remove()
+        if (flag && currCasualMenu !== casualMenus[index]) {
+            casualMenus[index - counter].remove()
             counter++
         }
     }
@@ -171,125 +219,166 @@ function removeNextSiblings(element,currCasualMenu,casualMenus){
     return casualMenus.length
 }
 
-function generateMenu(data,config,index) {
-    let length = document.getElementsByClassName(config.casualIndex).length > 0 ? document.getElementsByClassName(config.casualIndex)[0].getElementsByClassName(CASUAL_MENU_CLASS).length : 0,
+function generateMenu(data, config, index) {
+    let length = getElementsByClassName(config.casualIndex, 'much').length > 0 ? getElementsByClassName(config.casualIndex).getElementsByClassName(CASUAL_MENU_CLASS).length : 0,
         temp = [],
         counter = '',
         total = 0,
         row = {}
 
     index = index || ''
-    temp.push(CASUAL_MENU+CASUAL_MENU_LIST)
-    if(length !== 0){
+    temp.push('<div class="casual-menu" style="background-color: ' + config['backgroundColor'] + ';min-width: ' + config['width'] + ';border-right: 1px solid ' + config['borderColor'] + ';"><ul style="max-height: ' + config['height'] + '" class="casual-menu-list">')
+    if (length !== 0) {
         index += '-'
     }
 
-    for (let key in data){
+    for (let key in data) {
         row = data[key]
         total = config.children in row ? row[config.children].length : 0
-        counter = config.children in row ? '（'+total+'）</span><span data-index="'+index+key+'" class="casual-menu-list-item-icon casual-icon-right"></span>' : '</span>'
-        temp.push('<li class="casual-menu-list-item" data-index="'+index+key+'" data-count="'+total+'"><span data-index="'+index+key+'" class="casual-menu-list-item-text">'+row[config.visibility]+counter+'</li>')
+        counter = config.children in row ? '（' + total + '）</span><span data-index="' + index + key + '" class="casual-menu-list-item-icon casual-icon-right"></span>' : '</span>'
+        temp.push('<li class="casual-menu-list-item" data-value="'+row[config.visibility]+'" data-index="' + index + key + '" data-count="' + total + '"><span data-value="'+row[config.visibility]+'" data-index="' + index + key + '" class="casual-menu-list-item-text">' + row[config.visibility] + counter + '</li>')
     }
 
-    setTimeout(function(){
-        stopPropagation()
-    },300)
+    setTimeout(stopPropagation, 300)
 
     //casual-menu & casual-menu-list
-    temp.push(DIV_END+DIV_END)
+    temp.push(DIV_END + DIV_END)
 
     return temp
 }
 
 //防止误触关闭
-function stopPropagation(){
-    let collections = document.getElementsByClassName(CASUAL_MENU_LIST_CLASS)
-    for (let collection of collections){
-        collection.addEventListener('click',function(e){
+function stopPropagation() {
+    let collections = getElementsByClassName(CASUAL_MENU_LIST_CLASS, 'much')
+    for (let collection of collections) {
+        collection.addEventListener('click', function (e) {
             e.stopPropagation()
         })
     }
-    collections = document.getElementsByClassName(CASUAL_MENU_CLASS)
-    for (let collection of collections){
-        collection.addEventListener('click',function(e){
+    collections = getElementsByClassName(CASUAL_MENU_CLASS, 'much')
+    for (let collection of collections) {
+        collection.addEventListener('click', function (e) {
             e.stopPropagation()
         })
     }
 }
 
-function together (event) {
-    let element = event.target,
-        currCasualMenu =  element.tagName === 'SPAN' ? element.parentNode.parentNode.parentNode : element.parentNode.parentNode,
-        casualMenus = element.tagName === 'SPAN' ? element.parentNode.parentNode.parentNode.parentNode.children: element.parentNode.parentNode.parentNode.children,
+function together(event) {
+    let element = event.target.tagName === 'SPAN' ? event.target.parentNode : event.target,
+        currCasualMenu = element.parentNode.parentNode,
+        casualMenus = currCasualMenu.parentNode.children,
         dataIndex = element.getAttribute('data-index').split('-'),
-        config = configs[currCasualMenu.parentNode.classList[1]],
+        config = casualConfigs[currCasualMenu.parentNode.classList[1]],
         data = config['data']
 
     //阻止事件冒泡
     event.stopPropagation()
     //首先删除后面的menu
-    removeNextSiblings(element,currCasualMenu,casualMenus)
-    //选中状态
-    active(element)
+    removeNextSiblings(element, currCasualMenu, casualMenus)
 
-    for (let i = 0; i < dataIndex.length;i++){
+    //如果是选中状态则不渲染
+    if (element.classList.contains(ACTIVE_CLASS)) {
+        element.classList.remove(ACTIVE_CLASS)
+        return
+    }
+
+    //选中状态
+    activeClass(element, config)
+
+    for (let i = 0; i < dataIndex.length; i++) {
         data = data[dataIndex[i]][config['children']]
     }
 
-    insertHTML(document.getElementsByClassName(config['casualIndex'])[0],'beforeend',generateMenu(data,config,dataIndex.join('-')).join(''))
+    insertHTML(getElementsByClassName(config['casualIndex']), 'beforeend', generateMenu(data, config, dataIndex.join('-')).join(''))
+
 }
 
-function insertHTML(element,position,content){
-    element.insertAdjacentHTML(position,content)
+function setValues(config) {
+    let actives = getElementsByClassName(config['casualIndex']).getElementsByClassName(ACTIVE_CLASS)
+    let index = 0
+    let temp = []
+
+    for (let item of actives) {
+        temp[index] = item.children[0].getAttribute('data-value')
+        index++
+    }
+
+    config['values'] = temp
+    config['value'] = temp.join(config['delimiter'])
+    config['element'].value = config['value']
+}
+
+function insertHTML(element, position, content) {
+    element.insertAdjacentHTML(position, content)
     bindEvent()
 }
 
-function bindEvent (){
-    let items = document.getElementsByClassName(CASUAL_MENU_LIST_ITEM_CLASS)
+function bindEvent() {
+    let items = getElementsByClassName(CASUAL_MENU_LIST_ITEM_CLASS, 'much')
 
-    for (let item of items){
-        if(parseInt(item.getAttribute('data-count')) > 0){
-            item.addEventListener('click',together)
-        }else{
-            item.addEventListener('click',last)
+    for (let item of items) {
+        if (parseInt(item.getAttribute('data-count')) > 0) {
+            item.addEventListener('click', together)
+        } else {
+            item.addEventListener('click', clickLast)
         }
     }
 }
 
-function last(event) {
+function clickLast(event) {
     let element = event.target,
-        currCasualMenu =  element.tagName === 'SPAN' ? element.parentNode.parentNode.parentNode : element.parentNode.parentNode,
-        config = configs[currCasualMenu.parentNode.classList[1]]
+        currCasualMenu = element.tagName === 'SPAN' ? element.parentNode.parentNode.parentNode : element.parentNode.parentNode,
+        config = casualConfigs[currCasualMenu.parentNode.classList[1]]
+
+    activeClass(element, config)
 
     //是否阻止事件冒泡
-    if(config.close === false){
+    if (config.close === false) {
         event.stopPropagation()
-    }else{
+    } else {
         hidePanel()
     }
 
-    active(element)
 }
 
-function active(element){
+function activeClass(element, config) {
     element = element.tagName === 'SPAN' ? element.parentNode : element
     let children = element.parentNode.children
     for (let child of children) {
-        if(child.classList.contains('casual-menu-list-item_active')){
-            child.classList.remove('casual-menu-list-item_active')
+        child['style']['color'] = config['color']
+        if (child.classList.contains(ACTIVE_CLASS)) {
+            child.classList.remove(ACTIVE_CLASS)
         }
     }
-    element.classList.add('casual-menu-list-item_active')
+    element.classList.add(ACTIVE_CLASS)
+    element['style']['color'] = config['activeColor']
 }
 
-function hidePanel(){
-    let panels = document.getElementsByClassName(CASUAL_PANEL_CLASS)
-    for (let panel of panels){
-        if(panel['style']['display'] !== 'none'){
+function hidePanel() {
+    let panels = getElementsByClassName(CASUAL_PANEL_CLASS, 'much')
+    for (let panel of panels) {
+        if (panel['style']['display'] !== 'none') {
             panel['style']['display'] = 'none'
         }
     }
-    for (let key in configs){
-        configs[key]['isShow'] = false
+    for (let key in casualConfigs) {
+        casualConfigs[key]['isShow'] = false
+        iconChanged(casualConfigs[key]['element'].nextSibling, 'down')
+        setValues(casualConfigs[key])
     }
+}
+
+function iconChanged(element, type) {
+    if (type === 'down') {
+        element.classList.remove(ICON_UP_CLASS)
+        element.classList.add(ICON_DOWN_CLASS)
+    } else {
+        element.classList.remove(ICON_DOWN_CLASS)
+        element.classList.add(ICON_UP_CLASS)
+    }
+}
+
+function getElementsByClassName(className, type) {
+    type = type || 'single'
+    return type === 'single' ? document.getElementsByClassName(className)[0] : document.getElementsByClassName(className)
 }
